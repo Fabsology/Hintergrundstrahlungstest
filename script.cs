3760bfcbf9bb4e570061a5c4e52e4cdcfc9f3462
Fabsology <102152781+Fabsology@users.noreply.github.com>
2022-08-10 11:51:33 +0200

    Create script.cs

---
+++ script.cs
@@ -0,0 +1,7 @@
+// Bitflip - Experiment. Bitte im Draußen ausführen!
+
+while(true) {
+    System.Console.WriteLine("Keine Hintergrundstrahlung!");
+}
+
+System.Console.WriteLine("Hintergrundstrahlung gefunden!");
